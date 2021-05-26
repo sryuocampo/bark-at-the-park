6b70c1cb372297d2487189bcec753b133569cf82
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-05-26 00:34:51 -0700

    Completed css and html for sign up page.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import './home.css';
 export default function Home(props) {
   return (
-    <>
-    </>
+    <div className="container">
+    <div className="home-page">
+      <div className="blue-box"></div>
+      <div className="green-box"></div>
+      <h1>Bark at the Park</h1>
+      <p>A place for your furry friend to make their own furry friend.</p>
+   </div>
+   </div>
   );
 }
